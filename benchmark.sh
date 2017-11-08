cadd12a136567a921c1ec92612b41cf41187b80d
Mohammad Sahihi <msahihi1@gwdg.de>
2017-11-08 14:35:15 +0100

    major updatep

--- benchmark.sh
+++ benchmark.sh
@@ -3,8 +3,8 @@
 # vim: ts=4 syntax= bash sw=4 sts=4 sr noet
 
 #!/bin/bash
-#set -x
-#set -e
+# set -x
+# set -e
 
 
 
@@ -36,79 +36,6 @@ Usage: $0 [options]
 EOF
 }
 
-#                                                                       #
-#          C R E A T E   O V E R L A Y   N E T W O R K                  #
-#                                                                       #
-ps=$(sudo docker -H :4000 ps --filter "name=dc-" -a -q)
-network() {
-sudo docker -H:4000 network rm caching_network
-
-network_name="caching_network"
-
-	echo -e "---> Netowrk name : $network_name"
-	network=$(sudo docker network ls -f NAME=$network_name -q)
-	if [ -z "$network" ];
-		then
-			sudo docker -H:4000 network create --opt com.docker.network.driver.mtu=1450 --driver overlay $network_name
-			echo -e "[+] Network created"
-		else
-			echo -e "---> Network Exist"
-	fi
-
-}
-
-#                                                                       #
-#          R E M O V I N G   A L L   C O N T A I N E R S                #
-#                                                                       #
-
-remove_all () {
-
-	if [ -n "$ps" ]
-	then
-		echo -e "\n[+] Stopping Previous containers"
-		sudo docker -H :4000 stop  $(sudo docker -H :4000 ps --filter "name=dc-" -a -q)
-		echo -e "\n[+] Removing Previous containers"
-		sudo docker -H :4000 rm -f $(sudo docker -H :4000 ps --filter "name=dc-" -a -q)
-	fi
-	unload_snap_plugin
-
-}
-
-#                                                                       #
-#           B E N C H M A R K   S E R V E R S   N O D E S               #
-#                                                                       #
-
-create_server () {
-
-
-	echo -e "\n[+] Creating Servers\n"
-	for i in $(seq 1 1 $n)
-	do
-		sudo docker -H :4000 run --name dc-server$i --hostname dc-server$i -e constraint:node!=$(hostname) --network $network_name -d cloudsuite/data-caching:server -t $tt -m $mm -n $nn
-		echo -e "[+] Server $i is ready"
-	done
-
-}
-
-
-#                                                                       #
-#               B E N C H M A R K  C L I E N T  N O D E                 #
-#                                                                       #
-
-create_client () {
-
-      # By default docker swarm manager join as docker swarm node \
-			# to host cloudsuite datacaching client container.
-
-	echo -e "\n[+]  Creating Client\n"
-
-	sudo docker -H :4000 run -itd --name dc-client --hostname dc-client -e constraint:node==$(hostname) -v /var/log/benchmark:/home/log --network $network_name cloudsuite/data-caching:client bash
-	echo -e "[+] Client dc-client is ready\n"
-	sudo docker -H :4000 exec -d dc-client bash -c 'cd /usr/src/memcached/memcached_client/ && for i in $(seq 1 1 '"$n"'); do echo -e "dc-server$i, 11211" ; done > docker_servers.txt'
-
-
-}
-
 
 #                                                                       #
 #                  R U N N I N G   B E N C H M A R K                    #
@@ -123,45 +50,6 @@ run_benchmark () {
 	echo -e "[!] It may takes few minutes."
 	# Scaling the dataset and warming up the server
 	sudo docker -H :4000 exec -d dc-client bash -c 'cd /usr/src/memcached/memcached_client/ &&  stdbuf -o0 ./loader -a ../twitter_dataset/twitter_dataset_unscaled -o ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -w '"$w"' -S '"$S"' -D '"$D"' -j -T '"$T"' >> /home/log/warmup.log && stdbuf -o0 ./loader -a ../twitter_dataset/twitter_dataset_30x -s docker_servers.txt -g '"$g"' -T '"$T"' -c '"$c"' -w '"$w"' -t '"$t"' >> /home/log/benchmark.log'
-
-}
-
-#                                                                       #
-#               U N L O A D   S N A P   P L U G I N                     #
-#                                                                       #
-
-unload_snap_plugin () {
-
-echo -e "[+] Unloading Previous Collector Plugin ....."
-snaptel plugin unload processor tag 3 > /dev/null
-echo -e "[X] Tag Processor Plugin unloaded"
-snaptel plugin unload publisher influxdb 17 > /dev/null
-echo -e "[X] Influxdb Publisher Plugin unloaded"
-snaptel plugin unload collector cloudsuite-dc 1 > /dev/null
-echo -e "[X] Cloudsuite-datacaching Collector Plugin unloaded\n"
-
-echo -e "[+] Removing Previous SNAP Task ....."
-snaptel task list | cut -f 1 | tail -n +2 | while read LINE
-do
-	snaptel task stop ${LINE} > /dev/null
-	snaptel task remove ${LINE} > /dev/null
-	echo -e "---> Task ${LINE} removed"
-done
-}
-
-#                                                                       #
-#   C A P T U R E   M E T R I C S   V I A   S N A P   P L U G I N S     #
-#                                                                       #
-
-load_snap_plugin(){
-
-echo -e "[+] Loading Collector Plugin ....."
-snaptel plugin load $PWD/asset/snap/snap-plugin-processor-tag > /dev/null
-echo -e "[*] Tag Processor Plugin Loaded"
-snaptel plugin load $PWD/asset/snap/snap-plugin-publisher-influxdb > /dev/null
-echo -e "[*] Influxdb Publisher Plugin Loaded"
-snaptel plugin load $PWD/asset/snap/snap-plugin-collector-cloudsuite-datacaching > /dev/null
-echo -e "[*] Cloudsuite-datacaching Collector Plugin Loaded\n"
 
 }
 
@@ -356,12 +244,6 @@ then
 	echo -e "                                         "
 	echo -e "+---------------------------------------+"
 
-	sudo rm -rf /var/log/benchmark/*
-	remove_all
-	network
-	create_server
-	create_client
-	load_snap_plugin
 	run_benchmark
 	while [ ! -f /var/log/benchmark/benchmark.log ];
 	do
